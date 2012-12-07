7193a469dd45739518e058d303e89526cb027300
Pato Lankenau <plankenau@gmail.com>
2012-12-06 23:02:31 -0600

    Updated Client code
    Now supports for Scanning of servers

--- src/Client.java
+++ src/Client.java
@@ -1,19 +1,38 @@
+package rpg;
 
+import java.util.Scanner;
 import java.net.*;
 import java.io.*;
 import static java.lang.System.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Client {
-
+    private final boolean DEBUG = true;
     private Socket socket = null;
     private DataInputStream console = null;
     private DataOutputStream streamOut = null;
     private DataInputStream streamIn = null;
 
-
+    public static void main(String args[]) throws UnknownHostException {
+//        Client client = new Client(19999);// = new Client("169.254.145.138",19999);
+//        client.send("Testing");
+    }
+    public Client(int serverPort){
+        try {
+            if (DEBUG)out.println("Scanning for servers...");
+            String serverName = scan();
+            if (DEBUG)out.println("Server ["+serverName+"] found! Connecting...");
+            socket = new Socket(serverName, serverPort);
+            if (DEBUG)out.println("Connected: " + socket);
+            start();
+        } catch (UnknownHostException uhe) {
+            System.out.println("Host unknown: " + uhe.getMessage());
+        } catch (IOException ioe) {
+            System.out.println("Unexpected exception: " + ioe.getMessage());
+        }
+    }
     public Client(String serverName, int serverPort) {
-        out.println("Client started.");
-        System.out.println("Establishing connection. Please wait ...");
         try {
             socket = new Socket(serverName, serverPort);
             System.out.println("Connected: " + socket);
@@ -24,7 +43,28 @@ public class Client {
             System.out.println("Unexpected exception: " + ioe.getMessage());
         }
     }
-
+    public String scan() throws UnknownHostException{
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String root = ip.substring(0,ip.lastIndexOf(46));
+        int s = Integer.parseInt(ip.substring(ip.lastIndexOf(46)));
+        for (int i=s-30;i<255;i++){
+            boolean found = true;
+            String p ="";
+            try{
+                p = root+"."+i;
+                if (DEBUG)out.println(p);
+                Socket sock = new Socket();
+                sock.connect(new InetSocketAddress(p,19999),300);
+            }catch (UnknownHostException e){
+                found = false;
+            }catch (IOException e){
+                found = false;
+            }
+            if (found)
+                return p;
+        }
+        return null;
+    }
     public void send(String w){
         try{
             //out.println("begin send");
@@ -49,8 +89,7 @@ public class Client {
         }
         return line;
     }
-    @SuppressWarnings("deprecation")
-	public void readType(){
+    public void readType(){
         String line = "";
         while (!line.equals(".bye")){
             try {
@@ -62,13 +101,11 @@ public class Client {
             }
         }
     }
-
     public void start() throws IOException {
         console = new DataInputStream(System.in);
         streamOut = new DataOutputStream(socket.getOutputStream());
         streamIn = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
     }
-
     public void stop() {
         try {
             if (console != null) {
